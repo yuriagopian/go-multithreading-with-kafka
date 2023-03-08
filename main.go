a5aa6f341a974b1e9eb7ea31482114aeb423b73a
yuriagopian <yuri.agopian@hotmail.com>
2023-03-08 15:41:50 -0300

    chore: workers

--- main.go
+++ main.go
@@ -5,32 +5,20 @@ import (
 	"time"
 )
 
-func task(name string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(name, ":", i)
+func worker(workerId, data chan int) {
+	for value := range data {
+		fmt.Printf("Worker %d received %d\n", workerId, value)
 		time.Sleep(time.Second)
+
 	}
 }
 
 func main() {
-	channel := make(chan string)
+	ch := make(chan int)
+	qtsWorkers := 3
 
-	// thread 2
-	go func() {
-		channel <- "Olá Mundo!"
-	}()
-
-	// T1
-	msg := <-channel
-	fmt.Println(msg)
-
-	// go task("Tarefa 1") //T2 Nova thread
-	// go task("Tarefa 2") //T3 Nova thread
-	// task("Tarefa 3")    // T1
-}
-
-func publish(ch chan int) {
-	for i := 0; i < 20; i++ {
+	for i := 0; i < qtsWorkers; i++ {
 		ch <- i
 	}
+
 }
