c92a863629217330dfd6fd0e109328b7e823059e
yuriagopian <yuri.agopian@hotmail.com>
2023-03-08 17:43:00 -0300

    feat: kafka consumer

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/devfullcycle/gointesivo2/internal/infra/database"
+	"github.com/devfullcycle/gointesivo2/internal/usecase"
+)
 
 func main() {
 	db, err := sql.Open("sqlite3", "./orders.db")
@@ -8,5 +13,9 @@ func main() {
 		panic(err)
 	}
 
-	defer db.Close()
+	defer db.Close() // Executa tudo e depois executa o close
+
+	repository := database.NewOrderRepository(db)
+
+	usecase := usecase.CalculateFinalPrice{OrderRepository: repository}
 }
