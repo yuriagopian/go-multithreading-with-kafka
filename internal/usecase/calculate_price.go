176fc9aefbdd33ce913d57cd1ea544b362fb8020
yuriagopian <yuri.agopian@hotmail.com>
2023-03-08 17:03:00 -0300

    chore: workers load balancer

--- internal/usecase/calculate_price.go
+++ internal/usecase/calculate_price.go
@@ -19,4 +19,29 @@ type CalculateFinalPrice struct {
 	OrderRepository entity.OrderRepositoryInterface
 }
 
-func (c *CalculateFinalPrice) Execute(input OrderInputDTO) (OrderOutPuDTO, error) {}
+func (c *CalculateFinalPrice) Execute(input OrderInputDTO) (*OrderOutPuDTO, error) {
+	order, err := entity.NewOrder(input.ID, input.Price, input.Tax)
+
+	if err != nil {
+		return nil, err
+	}
+	err = order.CalculateFinalPrice()
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.OrderRepository.Save(order)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &OrderOutPuDTO{
+		ID:         order.ID,
+		Price:      order.Price,
+		Tax:        order.Tax,
+		FinalPrice: order.FinalPrice,
+	}, nil
+
+}
