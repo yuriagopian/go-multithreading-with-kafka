df1f8a2a7e5da3efb1ee729e6c73373761b30e96
yuriagopian <yuri.agopian@hotmail.com>
2023-03-08 22:22:58 -0300

    feat: kafka consumer

--- cmd/opencv/main.go
+++ cmd/opencv/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "gocv.io/x/gocv"
+import (
+	"image/color"
+
+	"gocv.io/x/gocv"
+)
 
 func main() {
 
@@ -12,7 +16,29 @@ func main() {
 	defer webcam.Close()
 
 	window := gocv.NewWindow("Detector")
-
 	defer window.Close()
 
+	classifier := gocv.NewCascadeClassifier()
+	defer classifier.Close()
+	classifier.Load("haarcascade_frontalface_default.xml")
+
+	for {
+		img := gocv.NewMat()
+		if ok := webcam.Read(&img); !ok {
+			println("Cannot read device")
+			return
+		}
+		if img.Empty() {
+			continue
+		}
+
+		myface := classifier.DetectMultiScale(img)
+		for _, r := range myface {
+			gocv.Rectangle(&img, r, color.RGBA{255, 0, 0, 0}, 2)
+		}
+		window.IMShow(img)
+		if window.WaitKey(1) >= 0 {
+			break
+		}
+	}
 }
